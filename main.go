b317a3a3887ac20335ba04f82b738b722369b780
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2022-11-11 20:31:35 +0900

    added first handler

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
+	handler1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Welcome My Page!\n")
+	}
+
+	http.HandleFunc("/new/", handler1)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
