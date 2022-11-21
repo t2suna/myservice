4aa04644537239d455463938caa67dbf89f0ae5e
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2022-11-21 23:24:54 +0900

    daily

--- main.go
+++ main.go
@@ -1,17 +1,75 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
-	handler1 := func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Welcome My Page!\n")
+	const (
+		noTmpHTML = "<html><body><h1>NO TEMPLATE.</h1></body></html>"
+		mail1     = "t2suna@gmail.com"
+		pass1     = "password"
+	)
+
+	tf, er := template.ParseFiles("templates/hello.html")
+	if er != nil {
+		tf, _ = template.New("index").Parse(noTmpHTML)
 	}
 
-	http.HandleFunc("/new/", handler1)
+	handler1 := func(w http.ResponseWriter, _ *http.Request) {
+/*		if msg != "" {
+			item := struct {
+				Title   string
+				Message string
+			}{
+				Title:   "Send Values",
+				Message: msg,
+			}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+			er = tf.Execute(w, item)
+			if er != nil {
+				log.Fatal(er)
+			}
+		} else {
+			er = tf.Execute(w, nil)
+			if er != nil {
+				log.Fatal(er)
+			}
+		}
+		*/
+		er = tf.Execute(w, nil)
+		if er != nil {
+			log.Fatal(er)
+
+	}
+
+	tf2, er := template.ParseFiles("templates/home.html")
+	if er != nil {
+		tf, _ = template.New("index").Parse(noTmpHTML)
+	}
+
+	handler2 := func(w http.ResponseWriter, rq *http.Request) {
+		mail := rq.FormValue("account")
+		pass := rq.FormValue("pass")
+		println(mail, pass)
+		if mail == mail1 && pass == pass1 {
+
+			er = tf2.Execute(w, nil)
+			if er != nil {
+				log.Fatal(er)
+			}
+
+		} else {
+			handler1(w, rq, mail+pass)
+			//エラーの渡し方を考えておく
+		}
+
+	}
+
+	http.HandleFunc("/new/", handler1, )
+	http.HandleFunc("/login/", handler2)
+
+	log.Fatal(http.ListenAndServe("", nil))
 }
