d87a7ec0a8b09e0b441d956764d720d9cc106696
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2022-12-09 21:39:05 +0900

    エラー表示を追加

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/t2suna/myservice
 go 1.18
 
 require github.com/gomodule/redigo v1.8.9
+
+require (
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
 
-	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/sessions"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// From https://qiita.com/akubi0w1/items/8701c05fe7186ceee632
-// Connection
-func Connection() (redis.Conn, error) {
-	const Addr = "127.0.0.1:6379"
+var cs *sessions.CookieStore = sessions.NewCookieStore([]byte("secret in my heart"))
 
-	c, err := redis.Dial("tcp", Addr)
+func Connection() (*sql.DB, error) {
+
+	db, err := sql.Open("sqlite3", "./test.db")
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
-	return c, nil
+	return db, nil
+}
+func ErrorSave(w http.ResponseWriter, rq *http.Request, err string, ses *sessions.Session) {
+	ses.Values["error"] = err
+	ses.Save(rq, w)
+
 }
 
 func main() {
+
 	const (
 		noTmpHTML = "<html><body><h1>NO TEMPLATE.</h1></body></html>"
-		mail1     = "t2suna@gmail.com"
-		pass1     = "password"
 	)
 
 	tf, er := template.ParseFiles("templates/hello.html")
@@ -32,32 +37,29 @@ func main() {
 		tf, _ = template.New("index").Parse(noTmpHTML)
 	}
 
-	handler1 := func(w http.ResponseWriter, _ *http.Request) {
-		/*		if msg != "" {
-					item := struct {
-						Title   string
-						Message string
-					}{
-						Title:   "Send Values",
-						Message: msg,
-					}
+	handler1 := func(w http.ResponseWriter, rq *http.Request) {
+		ses, _ := cs.Get(rq, "Session")
+		if ses.Values["error"] != nil {
+			item := struct {
+				Error string
+			}{
+				Error: ses.Values["error"].(string),
+			}
 
-					er = tf.Execute(w, item)
-					if er != nil {
-						log.Fatal(er)
-					}
-				} else {
-					er = tf.Execute(w, nil)
-					if er != nil {
-						log.Fatal(er)
-					}
-				}
-		*/
-		er = tf.Execute(w, nil)
-		if er != nil {
-			log.Fatal(er)
+			er = tf.Execute(w, item)
+			if er != nil {
+				log.Fatal(er)
+
+			}
+		} else {
+			er = tf.Execute(w, nil)
+			if er != nil {
+				log.Fatal(er)
+
+			}
 
 		}
+
 	}
 
 	tf2, er := template.ParseFiles("templates/home.html")
@@ -66,36 +68,40 @@ func main() {
 	}
 
 	handler2 := func(w http.ResponseWriter, rq *http.Request) {
+		ses, _ := cs.Get(rq, "Session")
+
 		if rq.Method == "POST" {
+
+			db, err := Connection()
+			if err != nil {
+				ErrorSave(w, rq, err.Error(), ses)
+				handler1(w, rq)
+			}
+			defer db.Close()
 			mail := rq.PostFormValue("account")
 			pass := rq.PostFormValue("pass")
 
-			// 接続
-			c, err := Connection()
-			if err != nil {
-				log.Fatal(er)
-			}
-			defer c.Close()
-			res_get, err := redis.String(c.Do("GET", mail))
-			if err != nil {
-				handler1(w, rq)
-				return
+			var count = 0
+
+			if err := db.QueryRow("select count(*) from users where mail = ? and pass = ?", mail, pass).Scan(&count); err != nil {
+				ErrorSave(w, rq, err.Error(), ses)
 			}
 
-			if pass == res_get {
+			if count > 0 {
 
 				er = tf2.Execute(w, nil)
 				if er != nil {
-					log.Fatal(er)
+					ErrorSave(w, rq, err.Error(), ses)
+					handler1(w, rq)
 				}
 
 			} else {
+				ErrorSave(w, rq, "会員情報が間違っています。", ses)
 				handler1(w, rq)
-				//エラーの渡し方を考えておく
 			}
 		} else {
+			ErrorSave(w, rq, "不正なリクエスト", ses)
 			handler1(w, rq)
-			//エラーの渡し方を考えておく
 		}
 
 	}
